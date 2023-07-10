be17b5716db01f48ba491081501093f0897895fa
nerdyparody <yash@bluerocksoft.com>
2023-07-10 11:56:30 -0700

    may have created a debian package but yet to be tested

---
+++ deb_dist/digihat-display-1.0/setup.py
@@ -0,0 +1,29 @@
+import os
+import io
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+classifiers = ['Development Status :: 4 - Beta',
+               'Operating System :: POSIX :: Linux',
+               'License :: OSI Approved :: MIT License',
+               'Intended Audience :: Developers',
+               'Programming Language :: Python :: 2.7',
+               'Programming Language :: Python :: 3',
+               'Topic :: Software Development',
+               'Topic :: System :: Hardware']
+
+setup(name              = 'Digihat_display',
+      version           = '1.0',
+      author            = 'Ben Payne, Yash Gandhi',
+      author_email      = 'yash@bluerocksoft.com',
+      description       = 'python code for digital hat display',
+      long_description  = long_description,
+      license           = 'MIT',
+      classifiers       = classifiers,
+      url               = 'https://github.com/InterludeAudio/digihat_display',
+      packages          = find_packages())
---
+++ deb_dist/digihat-display_1.0-1.dsc
@@ -0,0 +1,20 @@
+Format: 3.0 (quilt)
+Source: digihat-display
+Binary: python3-digihat-display
+Architecture: all
+Version: 1.0-1
+Maintainer: Ben Payne, Yash Gandhi <yash@bluerocksoft.com>
+Homepage: https://github.com/InterludeAudio/digihat_display
+Standards-Version: 3.9.1
+Build-Depends: python3-setuptools, python3-all, debhelper (>= 9)
+Package-List:
+ python3-digihat-display deb python optional arch=all
+Checksums-Sha1:
+ 22122d63af122dce8df790e35e93db93012cb939 5139 digihat-display_1.0.orig.tar.gz
+ 01e84de4cfe98f25ba183752ac669f15b5dd1fde 1160 digihat-display_1.0-1.debian.tar.xz
+Checksums-Sha256:
+ 493f7ce0e880aad45d942004fb0c7e6cdabceb96e08e4d71d71e74786ca95b96 5139 digihat-display_1.0.orig.tar.gz
+ 66309eed12f175aeaa8f4cf7617fb418929a9008eaddc640b6922801c6439a24 1160 digihat-display_1.0-1.debian.tar.xz
+Files:
+ d0ba232684659840f74c37b2dab555d1 5139 digihat-display_1.0.orig.tar.gz
+ 2f96581d3cf6846373bcdb1ecdc5ebbc 1160 digihat-display_1.0-1.debian.tar.xz
